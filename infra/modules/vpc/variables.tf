77d9b823ce52efbccdfa363cb09b8ea70391e73d
msalem05 <salemmohammed571@gmail.com>
2025-09-27 22:53:45 +0100

    Configured vpc module

--- infra/modules/vpc/variables.tf
+++ infra/modules/vpc/variables.tf
@@ -1,11 +1,3 @@
-#S3 Variables
-variable "bucket_name" {
-    type = string
-    default = "umami-tfstate"
-}
-
-#VPC Variables
-
 variable "vpc_cidr" {
     type = string
     default = "10.0.0.0/16"
