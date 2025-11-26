3a323ea2f8ff829388d1e9dedce793ba0aa82172
msalem05 <salemmohammed571@gmail.com>
2025-11-26 04:37:46 +0000

    Made vpc cidr variable a list

--- infra/modules/alb/main.tf
+++ infra/modules/alb/main.tf
@@ -19,7 +19,7 @@ resource "aws_security_group" "alb" {
         from_port = 80
         to_port = 80
         protocol = "tcp"
-        cidr_blocks = var.vpc_cidr
+        cidr_blocks = [var.vpc_cidr]
         
     }
 
@@ -34,7 +34,7 @@ resource "aws_security_group" "alb" {
         from_port = 443
         to_port = 443
         protocol = "tcp"
-        cidr_blocks = var.vpc_cidr
+        cidr_blocks = [var.vpc_cidr]
 
     }
 
@@ -50,7 +50,7 @@ resource "aws_security_group" "alb_ssh" {
         from_port = 22
         to_port = 22
         protocol = "tcp"
-        cidr_blocks = var.vpc_cidr
+        cidr_blocks = [var.vpc_cidr]
     }
 
     egress {
