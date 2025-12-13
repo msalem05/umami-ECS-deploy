fb6f5f9168d393454c06028c8e1fced82d28ab41
msalem05 <salemmohammed571@gmail.com>
2025-12-13 10:16:49 +0000

    Reconfigured entire iam module

--- infra/modules/iam/main.tf
+++ infra/modules/iam/main.tf
@@ -1,70 +1,74 @@
-data "aws_iam_policy_document" "assume_role_policy" {
-    statement {
-        actions = ["sts:AssumeRole"]
-
-        principals {
-          type = "Service"
-          identifiers = ["ecs-tasks.amazonaws.com"]
-        }
-    }
-
-}
-
 resource "aws_iam_role" "ecs_task_execution_role" {
     name = var.ecs_task_execution_role_name
-    assume_role_policy = data.aws_iam_policy_document.assume_role_policy.json
+    assume_role_policy = jsonencode({
+        Version = "2012-10-17"
+        Statement = [
+            {
+                Action = "sts:AssumeRole"
+                Effect = "Allow"
+                Principal = {
+                    Service = "ecs-tasks.amazonaws.com"
+                }
+            }
+        ]
+    })
 
 }
 
-data "aws_iam_policy_document" "ecs_task_execution" {
-    statement {
-        effect = "Allow"
-        actions = [
-            "ecr:GetAuthorizationToken",
-        ]
-        resources = ["*"]
-    }
-
-    statement {
-        effect = "Allow"
-        actions = [
-            "ecr:BatchGetImage",
-            "ecr:GetDownloadUrlForLayer",
-            "ecr:BatchCheckLayerAvailability"
-        ]
-        resources = [var.ecr_repository_arn]
-    }
-}
-
-resource "aws_iam_policy" "ecs_task_execution_policy" {
+resource "aws_iam_role_policy" "ecs_task_execution" {
     name = var.ecs_task_execution_policy_name
-    policy = data.aws_iam_policy_document.ecs_task_execution.json
-}
+    role = aws_iam_role.ecs_task_execution_role.id
 
-resource "aws_iam_role_policy_attachment" "ecs_task_execution" {
-    role = aws_iam_role.ecs_task_execution_role.name
-    policy_arn = aws_iam_policy.ecs_task_execution_policy.arn
+    policy = jsonencode({
+        Version = "2012-10-17"
+        Statement = [
+            {
+                Effect = "Allow"
+                Action = [
+                    "ecr:BatchGetImage",
+                    "ecr:GetDownloadUrlForLayer",
+                    "ecr:GetAuthorizationToken"
+                ]
+                Effect = "Allow"
+                Resource = "*"
+            }
+        ]
+    })
 }
 
 resource "aws_iam_role" "ecs_task_role" {
     name = var.ecs_task_role_name
-    assume_role_policy = data.aws_iam_policy_document.assume_role_policy.json
+    assume_role_policy = jsonencode({
+        Version = "2012-10-17"
+        Statement = [
+            {
+                Action = "sts:AssumeRole"
+                Effect = "Allow"
+                Principal = {
+                    Service = "ecs-tasks.amazonaws.com"
+                }
+
+            }
+        ]
+    })
 }
 
-data "aws_iam_policy_document" "ecs_task" {
-    statement {
-        effect = "Allow"
-        actions = []
-        resources = ["*"]
-    }
-}
-
-resource "aws_iam_policy" "ecs_task_policy" {
+resource "aws_iam_role_policy" "ecs_task" {
     name = var.ecs_task_policy_name
-    policy = data.aws_iam_policy_document.ecs_task.json
-}
-
-resource "aws_iam_role_policy_attachment" "ecs_task" {
-    role = aws_iam_role.ecs_task_role.name
-    policy_arn = aws_iam_policy.ecs_task_policy.arn
+    role = aws_iam_role.ecs_task_role.id
+    
+    policy = jsonencode({
+        Version = "2012-10-17"
+        Statement = [
+            {
+                Effect = "Allow"
+                Action = [
+                    "logs:CreateLogGroup",
+                    "logs:CreateLogStream",
+                    "logs:PutLogEvents"
+                ]
+                Resource = "*"
+            }
+        ]
+    })
 }
