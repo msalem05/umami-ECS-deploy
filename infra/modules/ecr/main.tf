e47bc3bf7aa808edb17743d6588e3c5b434e5003
msalem05 <salemmohammed571@gmail.com>
2025-12-15 10:20:33 +0000

    Added ecr lifecycle policy

--- infra/modules/ecr/main.tf
+++ infra/modules/ecr/main.tf
@@ -5,5 +5,26 @@ resource "aws_ecr_repository" "repo" {
         scan_on_push = true
     }
 
+    encryption_configuration {
+      encryption_type = "AE256"
+    }
+}
 
+resource "aws_ecr_lifecycle_policy" "" {
+    repository = aws_ecr_repository.repo.name
+
+    policy = jsonencode({
+        rules = [
+            {
+            rulePriority = 1
+            description = "Keep last 10 images"
+            selection = {
+                tagStatus = "any"
+                countType = "imageCountMoreThan"
+                countNumber = 10
+            }
+            Action = { type = "expire" }
+            }
+        ]
+    })
 }
