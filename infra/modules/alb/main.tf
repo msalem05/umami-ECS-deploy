3be4b098afee2b854c22b21761c6dca54608c301
msalem05 <salemmohammed571@gmail.com>
2025-12-29 10:37:43 +0000

    fixed sg ordering issue

--- infra/modules/alb/main.tf
+++ infra/modules/alb/main.tf
@@ -52,7 +52,7 @@ resource "aws_security_group" "alb" {
     from_port       = 3000
     to_port         = 3000
     protocol        = "tcp"
-    security_groups = [var.ecs_task_sg_id]
+    cidr_blocks     = ["0.0.0.0/0"]
   }
 }
 
