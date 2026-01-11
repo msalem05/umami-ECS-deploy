74cc8c8e8333777c4732cf8068c21a25deda7a79
msalem05 <salemmohammed571@gmail.com>
2026-01-11 18:55:38 +0000

    fixed formatting

--- infra/modules/iam/main.tf
+++ infra/modules/iam/main.tf
@@ -58,7 +58,7 @@ resource "aws_iam_role_policy" "ecs_task_execution" {
       }
     ]
   })
-  depends_on = [ aws_iam_role.ecs_task_execution_role ]
+  depends_on = [aws_iam_role.ecs_task_execution_role]
 }
 
 data "aws_caller_identity" "account" {
@@ -98,7 +98,7 @@ resource "aws_iam_role_policy" "ecs_task" {
       }
     ]
   })
-  depends_on = [ aws_iam_role.ecs_task_role ]
+  depends_on = [aws_iam_role.ecs_task_role]
 }
 
 resource "aws_iam_role" "rds_enhanced_monitoring" {
