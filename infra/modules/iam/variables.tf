2b564ebc3bf350b0b0030060054ed179e7da82fb
msalem05 <salemmohammed571@gmail.com>
2026-01-13 23:48:14 +0000

    removed on push triggers

--- infra/modules/iam/variables.tf
+++ infra/modules/iam/variables.tf
@@ -33,8 +33,3 @@ variable "db_id" {
 variable "db_username" {
   type = string
 }
-
-variable "rds_enhanced_monitoring_role_name" {
-  type    = string
-  default = "rds-enhanced-monitoring-role"
-}
